e0e671b38a7483b4a97745ba17dafdb056a117bc
Bona Akubue <bonaakubue@gmail.com>
2023-03-16 10:06:01 +0100

    operators in python

---
+++ ex1.py
@@ -0,0 +1,33 @@
+#arithmetic operators
+
+num1 = 20
+
+num2 = 5
+
+#addition operator
+result =  num1 + num2
+print(result)
+
+#subtraction operator
+result =  num1 - num2
+print(result)
+
+#multiplication
+result =  num1 * num2
+print(result)
+
+#division
+result =  num1 / num2
+print(result)
+
+#integer division
+result =  num1 // num2
+print(result)
+
+#modulus operator
+result =  num1 % num2
+print(result)
+
+#exponentiation operator
+result =  num1 ** num2
+print(result)
